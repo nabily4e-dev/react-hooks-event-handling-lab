19aea2fe88f426a7a24f4edac1b1698babd41b49
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-18 11:13:43 +0300

    EyesOnMe 2nd attempt

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,8 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
 function EyesOnMe() {
-    return (
-        <div></div>
-    )
+  return (
+    <div>
+      <button
+        onFocus={() => console.log('Good!')}
+        onBlur={() => console.log('Hey! Eyes on me!')}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
 }
 
 export default EyesOnMe;
